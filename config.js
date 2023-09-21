98967aebe7a73ffad660ff33fea5e6e645c3e02b
Satvik Manchanda <101499823+Satvik1769@users.noreply.github.com>
2023-09-21 22:21:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const secret = crypto.randomBytes(32).toString('hex');
 console.log("secret: ",secret);
 const blockchain_api_key_id = "ut4tggRw3sX2pMm";
 const blockchain_api_secret_key = "AqWw3XJZQrRu3ca";
-const personal_access_token = "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"
+const personal_access_token = "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"
 
 
 // const solana_secret_phase = 
