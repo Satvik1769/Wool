a95c2cf44e2383890babbbe0b7292b373ab8c85b
Satvik1769 <manchandasatvik@gmail.com>
2023-09-22 12:30:07 +0530

    update

---
+++ config.js
@@ -0,0 +1,15 @@
+const crypto = require('crypto');
+const secret = crypto.randomBytes(32).toString('hex');
+console.log("secret: ",secret);
+const blockchain_api_key_id = "ut4tggRw3sX2pMm";
+const blockchain_api_secret_key = "AqWw3XJZQrRu3ca";
+const personal_access_token = "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"
+
+
+// const solana_secret_phase = 
+module.exports = {
+    JWT_SECRET : secret,
+    BLOCKCHAIN_API_KEY_ID :blockchain_api_key_id ,
+    BLOCKCHAIN_API_SECRET_KEY : blockchain_api_secret_key,
+    PERSONAL_ACCESS_TOKEN : personal_access_token
+}
